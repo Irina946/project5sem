efc24c7d14683054247c36ca3f99bf6075f9fd5f
Irina946 <Irina.Larina@urfu.me>
2023-12-06 18:53:01 +0500

    Page create

--- src/ui-elements/button/button.jsx
+++ src/ui-elements/button/button.jsx
@@ -6,9 +6,10 @@ const Button = (props) => {
     const shadowClass = props.shadow === 'shadow' ? styles.shadow : '';
     const sizeClass = props.size === 'big' ? styles.big : '';
     const colorClass = props.color === 'orange' ? styles.orange : 'empty' ? styles.empty : '';
+    const active = props.active === 'active' ? styles.active : '';
 
     return (
-        <button className={clsx(styles.myButton, shadowClass, sizeClass, colorClass)} >
+        <button className={clsx(styles.myButton, shadowClass, sizeClass, colorClass, active)} onClick={props.click}>
             {props.title}
         </button>
     );
