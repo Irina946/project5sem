74a0a10db9a247c0156378a96e2953006f049daa
Irina946 <Irina.Larina@urfu.me>
2023-12-18 15:22:24 +0500

    trancport

--- src/create/create.jsx
+++ src/create/create.jsx
@@ -7,6 +7,7 @@ import differentialCalculusOne from "./differentialCalculusOne.json"
 import differentialCalculusSeveral from "./differentialCalculusSeveral.json"
 import Button from "../ui-elements/button/button";
 import clsx from "clsx";
+import { sendTasks } from "./transport";
 
 
 function getRandomComplexity(min, max) {
@@ -16,24 +17,47 @@ function getRandomComplexity(min, max) {
 
 
 const ButtonClick = (props) => {
-    const [isActive, setIsActive] = useState('');
+    const [selectedButtonId, setSelectedButtonId] = useState(props.id);
 
-    const handleClick = (button) => {
-        setIsActive(button);
+    const handleClick = (buttonID, complexity) => {
+        setSelectedButtonId(buttonID);
+        props.onChangeComplexity(complexity)
+
     }
+
+    
+
     const complexity = props.complexity;
     const easyNumber = getRandomComplexity(Number(complexity[0].easy[0]), Number(complexity[0].easy.at(-1)));
     const mediumNumber = getRandomComplexity(Number(complexity[0].medium[0]), Number(complexity[0].medium.at(-1)));
     const hardNumber = getRandomComplexity(Number(complexity[0].hard[0]), Number(complexity[0].hard.at(-1)));
-    console.log(easyNumber, mediumNumber, hardNumber)
-    return <div className={styles.buttons} ><Button title='Низкий' color="empty" value={easyNumber} click={() => handleClick(props.id + 1)} active={isActive === (props.id + 1) ? 'active' : ''}
-    /><Button title='Средний' value={mediumNumber} click={() => handleClick(props.id + 2)} active={isActive === (props.id + 2) ? 'active' : ''} /><Button title='Высокий' value={hardNumber} click={() => handleClick(props.id + 3)} active={isActive === (props.id + 3) ? 'active' : ''} /></div>
+
+    return (
+        <div className={styles.buttons} >
+            <Button
+                title='Низкий'
+                click={() => handleClick(props.id + 0, easyNumber)}
+                active={selectedButtonId === (props.id + 0) ? 'active' : ''}
+            />
+            <Button
+                title='Средний'
+                click={() => handleClick(props.id + 1, mediumNumber)}
+                active={selectedButtonId === (props.id + 1) ? 'active' : ''}
+            />
+            <Button
+                title='Высокий'
+                click={() => handleClick(props.id + 2, hardNumber)}
+                active={selectedButtonId === (props.id + 2) ? 'active' : ''}
+            />
+        </div>
+    );
 }
 
 
 
 function LeftDiv() {
-    const [buttonStyle, setButtonStyle] = useState('');
+
+    const [buttonStyle, setButtonStyle] = useState('button1');
     const [TaskData, setTaskData] = useState(linearAlgebra);
     const handleButtonClick = (buttonId) => {
         if (buttonId === 'button1') {
@@ -62,10 +86,27 @@ function LeftDiv() {
         checkChange.themes[idx].isVisible = !checkChange.themes[idx].isVisible;
         setCheck(checkChange)
     }
+
     
+
+    const handleChangeCount = (idx, count) => {
+        console.log(idx, count)
+        const checkChange = { ...check };
+        checkChange.themes[idx].count =  count;
+        setCheck(checkChange)
+    }
+
+    const handleClickComplexity = (idx, complexity) => {
+        const checkChange = { ...check };
+        console.log( checkChange.themes[idx]); 
+        checkChange.themes[idx].selectedComplexity = complexity;
+        setCheck(checkChange)
+    }
+
     useEffect(() => {
         setCheck(TaskData)
     }, [TaskData])
+
 
     return (<><div className={styles.leftDiv}>
         <img src={ImgLogo} alt='логотип' className={styles.logo} width="225" height="29"></img>
@@ -74,26 +115,48 @@ function LeftDiv() {
         <button id="button3" className={clsx(styles.theme, button3)} onClick={() => handleButtonClick("button3")}>Дифференциальное исчисление функции одной переменной</button>
         <button id="button4" className={clsx(styles.theme, button4)} onClick={() => handleButtonClick("button4")}>Дифференциальное исчисление функции нескольких переменных</button>
     </div>
-    <div className={styles.centerDiv}>
-    {check.themes.map((theme, idx) => (
-        <div className={styles.checkboxContainer}>
-            <input id={idx} type="checkbox" defaultChecked={theme.isVisible} onClick={() => checkClick(idx)} name="parameters" className={styles.customCheckbox} />
-            <label className={styles.label} htmlFor={idx}>{theme.title}</label>
+        <div className={styles.centerDiv}>
+            {check.themes.map((theme, idx) => (
+                <div className={styles.checkboxContainer}>
+                    <input
+                        id={idx}
+                        type="checkbox"
+                        defaultChecked={theme.isVisible}
+                        onClick={() => checkClick(idx)}
+                        name="parameters"
+                        className={styles.customCheckbox}
+                    />
+                    <label className={styles.label} htmlFor={idx}>{theme.title}</label>
+                </div>
+            ))}
         </div>
-    ))}
-</div>
         <div className={styles.rightDiv}>
-            {check.themes.filter((el) => el.isVisible).map((div, idx) => (
+            {check.themes.map((div, idx) => (
+                !div.isVisible ? <></> :
                 <div className={styles.oneTheme} id={idx + 100}>{div.title}
                     <div className={styles.parameters}>Выберите начальный уровень сложности</div>
-                    <ButtonClick id={idx + 1000} complexity={div.complexity} />
+                    <ButtonClick
+                        id={idx + 1000}
+                        complexity={div.complexity}
+                        onChangeComplexity={(complexity) => handleClickComplexity(idx, complexity)}
+                    />
                     <div className={styles.parameters}>Введите количество задач</div>
-                    <input className={styles.numberInput} type="number" min={1}></input>
+                    <input
+                        className={styles.numberInput}
+                        type="number"
+                        value={div.count}
+                        onChange={(e) => handleChangeCount(idx, e.target.value)}
+                    />
                 </div>
             ))}
         </div>
         <div className={styles.buttonContainer}>
-            <Button color="orange" size="big" title="Начать генерацию" />
+            <Button
+                color="orange"
+                size="big"
+                title="Начать генерацию"
+                click = {() => sendTasks(check)}
+            />
         </div></>)
 }
 
